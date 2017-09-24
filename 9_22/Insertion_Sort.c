6a2e1d5b88173c4347b172341c7279d649bd5748
QihaoYe <11610812@mail.sustc.edu.cn>
2017-09-24 15:24:33 +0800

    update

--- 9_22/Insertion_Sort.c
+++ 9_22/Insertion_Sort.c
@@ -23,21 +23,29 @@ int main(int argc, char const *argv[])
 {
 	clock_t start, finish;
 	int N = 10000;
+	// Set a large N
 	srand(time(NULL));
+	// Set a random seed
 	int a[N];
 	for (int i=0;i<N;i++)
 	{
-		a[i] = rand() % 100 + 1;
+		a[i] = rand() % 10000 + 1;
 	}
+	// Initialize a random array
 	start = clock();
 	insertion_sort(a,N);
+	// Insertion Sort
+	// Order: small -> large
 	finish = clock();
 	double duration = (double)(finish - start) / CLOCKS_PER_SEC;
+	// Get the total time of sorting
 	for (int i=0;i<N;i++)
 	{
 		printf("%d ", a[i]);
 	}
 	printf("\n");
+	// Print out the result
 	printf( "%f seconds\n", duration);
+	// Print out the time of sorting
 	return 0;
 }
--- 9_22/Merge_Sort.c
+++ 9_22/Merge_Sort.c
@@ -19,15 +19,21 @@ void merge(ElementType * A, int low, int mid, int high)
 }
 
 
-void merge_sort(ElementType * A, int low, int high)
+void merge_process(ElementType * A, int low, int high)
 {
 	if (low < high)
 	{
 		int mid = (low + high) / 2;
-		merge_sort(A, low, mid);
-		merge_sort(A, mid+1, high);
+		merge_process(A, low, mid);
+		merge_process(A, mid+1, high);
 		merge(A, low, mid, high);
 	}
+}
+
+
+void merge_sort(ElementType * A, int N)
+{
+	merge_process(A, 0, N-1);
 }
 
 
@@ -35,21 +41,29 @@ int main(int argc, char const *argv[])
 {
 	clock_t start, finish;
 	int N = 1000000;
+	// Set a large N
 	srand(time(NULL));
+	// Set a random seed
 	int a[N];
 	for (int i=0;i<N;i++)
 	{
-		a[i] = rand() % 100 + 1;
+		a[i] = rand() % 1000000 + 1;
 	}
+	// Initialize a random array
 	start = clock();
-	merge_sort(a, 0, N-1);
+	merge_sort(a, N);
+	// Merge Sort
+	// Order: small -> large
 	finish = clock();
 	double duration = (double)(finish - start) / CLOCKS_PER_SEC;
+	// Get the total time of sorting
 	for (int i=0;i<N;i++)
 	{
 		printf("%d ", a[i]);
 	}
 	printf("\n");
+	// Print out the result
 	printf( "%f seconds\n", duration);
+	// Print out the time of sorting
 	return 0;
 }
