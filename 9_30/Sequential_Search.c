6e8dff20b469d73ffd22bce0ec6f81f567a8d623
QihaoYe <11610812@mail.sustc.edu.cn>
2017-09-30 11:01:27 +0800

    update

---
+++ 9_30/Sequential_Search.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define ElementType int
+
+
+// void sequential_search(static Table *Tb1, ElementType k)
+// {
+// 	int i;
+// 	Tb1->Element[0] = k;
+// 	for (i=Tb1->length;Tb1->Element[i]!=k;i--)
+// 		return i;
+// }
+
+
+
+int main(int argc, char const *argv[])
+{
+	return 0;
+}
