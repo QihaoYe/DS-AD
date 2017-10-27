0109e06eb7ffde165296b9ecf8cd1746659a8b1f
QihaoYe <11610812@mail.sustc.edu.cn>
2017-10-27 18:08:44 +0800

    update

---
+++ 10_27/Linked_List.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define ElementType int
+
+
+typedef struct ListNode
+{
+	ElementType key;
+	struct ListNode* next;
+}ListNode;
+
+
+int main(int argc, char const *argv[])
+{
+	return 0;
+}
