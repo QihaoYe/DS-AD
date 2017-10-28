8e1f545e3c5a6e97eacee8f9895dfcb9f54c6b26
QihaoYe <11610812@mail.sustc.edu.cn>
2017-10-28 20:38:29 +0800

    update

--- 10_27/Linked_List.c
+++ 10_27/Linked_List.c
@@ -110,8 +110,7 @@ void delete(ListNode ** head, int i)
 			break;
 		}
 		ListNode * p2;
-		int n = 0;
-		while ((p2 = p1->next) && ++n < i) p1 = p2;
+		while ((p2 = p1->next) && --i) p1 = p2;
 		if (NULL == p2)
 		{
 			fprintf(stderr, "%s\n", "Index out of bound!");
@@ -134,9 +133,35 @@ bool search(ListNode * head, ElementType key)
 }
 
 
+ElementType * get_value_pointer(ListNode * head, int i)
+{
+	do
+	{
+		if (NULL == head)
+		{
+			fprintf(stderr, "%s\n", "Invalid head selected!");
+			break;
+		}
+		if (0 > i)
+		{
+			fprintf(stderr, "%s\n", "Invalid index selected!");
+			break;
+		}
+		while (i-- && (head = head->next));
+		if (NULL == head)
+		{
+			fprintf(stderr, "%s\n", "Index out of bound!");
+			break;
+		}
+		return &(head->value);
+	} while (0);
+	return NULL;
+}
+
+
 ElementType get_value(ListNode * head, int i)
 {
-
+	return *get_value_pointer(head, i);
 }
 
 
@@ -152,18 +177,89 @@ void print(ListNode * head)
 }
 
 
-void insert(ListNode * head, int i, ElementType key)
+void insert(ListNode ** head, int i, ListNode * node)
 {
-
+	do
+	{
+		if (NULL == *head)
+		{
+			fprintf(stderr, "%s\n", "Invalid head selected!");
+			break;
+		}
+		if (0 > i)
+		{
+			fprintf(stderr, "%s\n", "Invalid index selected!");
+			break;
+		}
+		ListNode * p1 = *(head);
+		if (i == 0)
+		{
+			node->next = p1;
+			*head = node;
+			break;
+		}
+		ListNode * p2;
+		while ((p2 = p1->next) && --i) p1 = p2;
+		if (NULL == p2 && i > 1)
+		{
+			printf("%d\n", i);
+			fprintf(stderr, "%s\n", "Index out of bound!");
+			break;
+		}
+		p1->next = node;
+		node->next = p2;
+	} while(0);
 }
 
 
 ListNode * merge(ListNode * A, ListNode * B)
 {
-
+	if (NULL == A) return B;
+	ListNode * p1 = A;
+	ListNode * p2;
+	while ((p2 = p1->next)) p1 = p2;
+	p1->next = B;
+	return A;
 }
 
 
+void reverse(ListNode ** head)
+{
+	ListNode* p1;
+	ListNode* p2;
+	if (NULL == (p1 = *head) || NULL == (p2 = (*head)->next)) return;
+	ListNode* p3;
+	(*head)->next = NULL;
+	while ((p3 = p2->next))
+	{
+		p2->next = p1;
+		p1 = p2;
+		p2 = p3;
+	}
+	p2->next = p1;
+	*head = p2;
+}
+
+
+ListNode * reversed(ListNode * head)
+{
+	if (NULL == head || NULL == head->next) return head;
+	ListNode* p1 = head;
+	ListNode* p2 = head->next;
+	ListNode* p3;
+	head->next = NULL;
+	while ((p3 = p2->next))
+	{
+		p2->next = p1;
+		p1 = p2;
+		p2 = p3;
+	}
+	p2->next = p1;
+	return p2;
+}
+
+
+// ---[test zone]---
 void test(ListNode * head)
 {
 	int n = 0;
@@ -173,15 +269,56 @@ void test(ListNode * head)
 
 int main(int argc, char const *argv[])
 {
-	ListNode * a;
+	ListNode * a, * b, * c = (ListNode *)malloc(sizeof(ListNode));
 	a = create(10);
+	b = create(5);
+	// c = create(1);
+	c->value = 999;
+	c->next = NULL;
 	test(a);
+	// Initialize
+	printf("%s", "a[] = ");
 	print(a);
-	printf("Length: %d\n", calculate_length(a));
+	printf("%s", "b[] = ");
+	print(b);
+	printf("%s", "c[] = ");
+	print(c);
+
+	printf("\n");
+	printf("a.length = %d\n", calculate_length(a));
+
+	printf("\n");
 	delete(&a, 6);
+	printf("%s\n", "---Delete a[6]---");
+	printf("%s", "a[] = ");
 	print(a);
-	printf("Length: %d\n", calculate_length(a));
-	printf("Search [3]: %d\n", search(a, 3));
+
+	printf("\n");
+	printf("Search 3 in a: %d\n", search(a, 3));
+	printf("Search 6 in a: %d\n", search(a, 6));
+	printf("Search 123 in a: %d\n", search(a, 123));
+
+	printf("\n");
+	printf("a[5] = %d\n", get_value(a, 5));
+
+	printf("\n");
+	printf("%s\n", "---Insert c at a[9]---");
+	insert(&a, 9, c);
+	print(a);
+	print(c);
+	// printf("%s\n", "---Insert c at a[4]---");
+	// insert(&a, 4, c);
+	// print(a);
+	// printf("%s\n", "---Insert c at a[0]---");
+	// insert(&a, 0, c);
+	// print(a);
+
+	a = merge(a, b);
+	print(a);
+	reverse(&a);
+	print(a);
+	a = reversed(a);
+	print(a);
 	destroy(&a);
 	print(a);
 	printf("Length: %d\n", calculate_length(a));
