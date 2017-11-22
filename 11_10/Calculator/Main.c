42ae74244014ddf7de42ec59cb37f709a14c92b7
QihaoYe <11610812@mail.sustc.edu.cn>
2017-11-22 17:55:36 +0800

    update

--- 11_10/Calculator/Main.c
+++ 11_10/Calculator/Main.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int cal_priority(char operation)
+{
+	switch (operation)
+	{
+		case '+':
+		case '-':return 1;break;
+		case '*':
+		case '/':return 2;break;
+		default:return -1;break;
+	}
+}
 
 
 int main(int argc, char const *argv[])
