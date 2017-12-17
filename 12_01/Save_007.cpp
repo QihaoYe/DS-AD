694e698c8fef4c98cc5842bfd1f9889420fa929e
Qihao Ye <11610812@mail.sustc.edu.cn>
2017-12-18 01:08:27 +0800

    update

--- 12_01/Save_007.cpp
+++ 12_01/Save_007.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#define BOUNDARY_X 50
+#define BOUNDARY_Y 50
 
 
 // bfs
@@ -7,4 +9,45 @@ int main(int argc, char const *argv[])
 {
 	
 	return 0;
+}
+
+
+void DFS(int V)
+{
+    Visited[V] = true;
+    printf("%d ", V);
+    for (int i = 0; i < n; i++)
+    {
+        if (G[V][i] && !Visited[i])
+            DFS(i);
+    }
+}
+
+void BFS(int V)
+{
+    const int MAX_SIZE = 100;
+    int Queue[MAX_SIZE];
+    int first = -1, last = -1;
+
+    Queue[++last] = V;      //入队
+    Visited[V] = true;
+    while (first < last)    //当队不为空时
+    {
+        int F = Queue[++first];     //出队
+        printf("%d ", F);
+        for (int i = 0; i < n; i++)
+        {
+            if (G[F][i] && !Visited[i])
+            {
+                Queue[++last] = i;      //入队
+                Visited[i] = true;
+            }
+        }
+    }
+}
+
+void InitVisit()
+{
+    for (int i = 0; i < N; i++)
+        Visited[i] = false;
 }
