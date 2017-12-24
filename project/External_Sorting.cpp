64175884ed56f09720af49c3efad43bd59adae9e
Qihao Ye <11610812@mail.sustc.edu.cn>
2017-12-24 14:19:17 +0800

    update

--- project/External_Sorting.cpp
+++ project/External_Sorting.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
-#include <regex>
+#include <fstream>
+#include <regex.h>
 #define MAX_LENGTH 64
 #define FatalError(code) fprintf(stderr, code)
-char FILENAME[] = "massive_float/test.txt";
+char FILENAME[] = "massive_float/data.txt";
 
 
 using namespace std;
 
 
-void read_in(FILE ** fp, const char * PATH, const char * filename)
+void get_absolute_path(char document[])
 {
-    char document[5 * MAX_LENGTH];
-    strcpy(document, PATH);
     for (int i=(int)strlen(document) - 1; i >= 0; i--)
     {
         if (document[i] == '/')
             break;
         document[i] = '\0';
     }
+}
+
+
+void read_in(FILE ** fp, const char * PATH, const char * filename)
+{
+    char document[5 * MAX_LENGTH];
+    strcpy(document, PATH);
+    get_absolute_path(document);
     strcat(document, filename);
     if (NULL == (*fp = fopen(document,"r")))
         FatalError("Can not open the file!\n");
@@ -55,23 +62,28 @@ void merge_sort(double A[], double temp[], int left, int right)
 
 int is_dirty_data(const char buff[])
 {
-    regex reg("^(-?\\d+)(\\.\\d+)?([eE][-+]?\\d+)?\\n$");
-    string observe = buff;
-    smatch result;
-    if (regex_match(observe, result, reg))
-        return 0;
-    return 1;
+    const char * pattern = "^(-?[0-9]+)([.][0-9]+)?([eE][-+]?[0-9]+)?[\n]$";
+    regex_t reg = {};
+    int flag = 0;
+    regcomp(&reg, pattern, REG_EXTENDED|REG_NOSUB);
+    flag = regexec(&reg, buff, 0, NULL, 0);
+    regfree(&reg);
+    if (flag)
+        return 1;
+    return 0;
 }
 
 
-int main(int argc, char const *argv[])
+void get_clean_data(FILE * data, const char * PATH, const char * filename)
 {
-    FILE * fp;
+    char document[5 * MAX_LENGTH];
     char buff[MAX_LENGTH];
-	argv[0] = "/Users/apple/Documents/Data_Structures/project/External_Sorting";
-    read_in(&fp, argv[0], FILENAME);
     int counter = 0;
-    while (fgets(buff, MAX_LENGTH, fp))
+    strcpy(document, PATH);
+    get_absolute_path(document);
+    strcat(document, filename);
+    ofstream out(document);
+    while (fgets(buff, MAX_LENGTH, data))
     {
         double x;
         int flag = 0;
@@ -84,18 +96,50 @@ int main(int argc, char const *argv[])
             break;
         }
         else
+        {
             counter++;
-        if ((counter + 1) % 100000 == 0)
+            out << x << endl;
+        }
+        if ((counter) % 100000 == 0)
             cout << counter << endl;
     }
+    out.close();
     cout << counter << endl;
+}
+
+/*
+ * abandoned
+ * reason: too slow
+#include <regex>
+int is_dirty_data(const char buff[])
+{
+    regex reg("^(-?\\d+)(\\.\\d+)?([eE][-+]?\\d+)?\\n$");
+    string observer = buff;
+    smatch result;
+    if (regex_match(observer, result, reg))
+        return 0;
+    return 1;
+}
+*/
+
+int main(int argc, char const *argv[])
+{
+    FILE * fp;
+	argv[0] = "/Users/apple/Documents/Data_Structures/project/External_Sorting";
+    read_in(&fp, argv[0], FILENAME);
+    get_clean_data(fp, argv[0], "output/clean_data.txt");
 	return 0;
 }
 
-//int main()
+//int main(int argc, char const *argv[])
 //{
-//    char a[] = "2gj3.234adsf";
-//    double b = strtod(a, nullptr);
-//    cout <<  << endl;
+//    ofstream out("/Users/apple/Documents/Data_Structures/project/output/out.txt");
+//    if (out.is_open())
+//    {
+//        out << "This is a line.\n";
+//        out << "This is another line.\n";
+//        cout << "ahas" << endl;
+//        out.close();
+//    }
 //    return 0;
 //}
